360b325c5909fd7ff11879b303ca04f683917b37
Antoine Brique <antoine.brique@gmail.com>
2021-11-23 18:00:40 +0100

    add ProjectsController action CREATE

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,26 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
+    @project.user = current_user
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    text = Text.new
+    @project.text = text
+
+    if @project.save
+      redirect_to text_path(@project.text)
+    else
+      redirect_to "new"
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
