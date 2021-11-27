5dee2a436e512925d2dce7220f16d587c8602c06
Antoine Brique <antoine.brique@gmail.com>
2021-11-27 17:46:06 +0100

    add toggle sidebar

--- app/javascript/components/toggleSidebar.js
+++ app/javascript/components/toggleSidebar.js
@@ -1,5 +1,12 @@
 const toggle = () => {
+  const arrow = document.getElementById("sideArrow");
+  const sidebar = document.getElementById("sidebar");
 
+  arrow.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    sidebar.classList.toggle("small")
+  })
 }
 
 export { toggle }
