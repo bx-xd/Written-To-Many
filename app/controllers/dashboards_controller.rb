42b1f7f8fada99e173067544f2fe74414455cb25
Louis Quenault <louis.quenault@laposte.net>
2021-11-29 16:33:38 +0100

    Add links from projects contributions card to discussions

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,20 +3,20 @@ class DashboardsController < ApplicationController
     @user = current_user
     @projects = @user.projects
     @contributions = @user.projects_contributions
-    #@character = compting_writing_character
+    @character = compting_writing_character
   end
 
-  #def compting_writing_character
-    #compteur = 0
-    #@projects.each do |project|
-      #compteur += project.text.content.length
-    #end
+  def compting_writing_character
+    compteur = 0
+    @projects.each do |project|
+      compteur += project.text.content.length
+    end
 
-    #@user.modifications.each do |modification|
-      #if (modification.content_after.length - modification.content_before.length).positive?
-        #compteur += modification.content_after.length - modification.content_before.length
-      #end
-    #end
-    #return compteur
-  #end
+    @user.modifications.each do |modification|
+      if (modification.content_after.length - modification.content_before.length).positive?
+        compteur += modification.content_after.length - modification.content_before.length
+      end
+    end
+    return compteur
+  end
 end
