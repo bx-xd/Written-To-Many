ed60dc848607ef9905e1a07fb224e0e6c78ddca4
Antoine Brique <antoine.brique@gmail.com>
2021-12-01 15:37:05 +0100

    remove timago in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("rails-timeago")
-require("rails-timeago-all")
 
 import "trix"
 import "@rails/actiontext"
