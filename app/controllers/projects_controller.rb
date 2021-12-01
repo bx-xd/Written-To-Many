e4ba85aa5866d0e61b8437e63d5c2ce42d0f2a80
Louis Quenault <louis.quenault@laposte.net>
2021-12-01 10:37:35 +0100

    Setup dashboard navbar on new project and stylise new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,6 +8,10 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
     @project.user = current_user
+    @user = @project.user
+    @contributions = @user.projects_contributions
+    @projects = @user.projects
+    @character = compting_writing_character
   end
 
   def create
@@ -27,5 +31,19 @@ class ProjectsController < ApplicationController
 
   def project_params
     params.require(:project).permit(:title, :description, :photo)
+  end
+
+  def compting_writing_character
+    compteur = 0
+    @projects.each do |project|
+      compteur += project.text.content.length unless project.text.content.nil?
+    end
+
+    @user.modifications.each do |modification|
+      if (modification.content_after.length - modification.content_before.length).positive?
+        compteur += modification.content_after.length - modification.content_before.length
+      end
+    end
+    return compteur
   end
 end
