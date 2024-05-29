2d09744de956712d6656025f3a991030e33d404d
Antoine Brique <antoine.brique@gmail.com>
2024-05-29 11:30:58 +0200

    :dizzy: Update toggle bar animations

--- app/javascript/components/toggleSidebar.js
+++ app/javascript/components/toggleSidebar.js
@@ -1,19 +1,11 @@
 const toggle = () => {
   const sidebar = document.getElementById("sidebar");
   const arrow = document.getElementById("sideArrow");
-  const projectTitle = document.getElementById('project_title');
-  const listTitles = document.getElementById('list_titles');
 
   if (sidebar) {
     arrow.addEventListener("click", (event) => {
       event.preventDefault();
-      console.log(event)
-      //sidebar.style.transfrom = 'translateX(-200px)'
       sidebar.classList.toggle("collapsed");
-      // sidebar.addEventListener('transitionend', () => {
-      //   projectTitle.style.opacity = 0;
-      //   listTitles.style.opacity = 1;
-      // });
     })
   }
 }
